539637ebec8b22ba9f9a47260c00d11afe35e3b9
Dhruv Rekhawat <hellodhruvr@gmail.com>
2026-06-01 09:54:21 +0530

    seed ATS

--- prisma/seed-treatments.ts
+++ prisma/seed-treatments.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pkg from 'pg'
 
-const prisma = new PrismaClient()
+const { Pool } = pkg
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 // Treatment data from ATS.txt
 const treatments = [
